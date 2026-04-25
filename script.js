c2c84d447c9b0c611e7ba0949fbeb48c8eb2ae4a
our-wedding-0607 <minjeong.choi@doosan.com>
2026-04-25 22:01:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -561,7 +561,10 @@
     $('#locationHall').textContent = w.hall;
     $('#locationAddress').textContent = w.address;
     $('#locationTel').textContent = w.tel ? `Tel. ${w.tel}` : '';
-   /* $('#locationMapImg').src = 'images/location/1.jpg'; */
+    const mapImg=$('#locationMapImg');
+    if(mapImg) {
+      mapImg.src='images/location/1.jpg';
+    }
     $('#kakaoMapBtn').href = w.mapLinks.kakao || '#';
     $('#naverMapBtn').href = w.mapLinks.naver || '#';
 
